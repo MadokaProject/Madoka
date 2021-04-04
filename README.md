d31d840b5d45db6c34cddcbc53c06441efeddc89
Colsrch <colsrch@foxmail.com>
2021-04-04 13:45:36 +0800

    修复聊天模块的异常处理，添加腾讯AI配置项，优化github监听模块，添加github监听消息推送群聊配置项，更新文档

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ### 使用方法
 
-本程序与数据库相关联，使用前需要先补全 `config.py` 文件
+本程序与数据库相关联，使用前需要先创建一个数据库，并补全 `app/core/env.config.py` → `app/core/config.py` 文件
 
 #### 克隆
 
@@ -29,3 +29,11 @@ sh run.sh
 ``` cmd
 python main.py
 ```
+
+### 鸣谢&相关项目
+
+- [`mirai`](https://github.com/mamoe/mirai): 即 `mirai-core`, 一个高性能, 高可扩展性的 QQ 协议库
+- [`mirai-console-loader`](https://github.com/iTXTech/mirai-console-loader): 模块化、轻量级且支持完全自定义的 `mirai` 加载器。
+- [`mirai-console`](https://github.com/mamoe/mirai-console): 一个基于 `mirai` 开发的插件式可扩展开发平台
+- [`mirai-api-http`](https://github.com/project-mirai/mirai-api-http): 提供与 `mirai` 交互方式的 `mirai-console` 插件
+- [`Graia Application`](https://github.com/GraiaProject/Application): 一个设计精巧, 协议实现完备的, 基于 `mirai-api-http` 的即时聊天软件自动化框架.
--- app/core/env.config.py
+++ app/core/env.config.py
@@ -2,23 +2,24 @@
 # 使用需将其重命名为config.py，并填写修改其中的配置
 
 # QQ账号基本配置
-HOST = 'http://example:port'  # 机器人mirar地址
-AUTHKEY = ''  # 机器人验证密钥
-QQ = 'bot qq'  # 机器人QQ
+HOST = 'http://example:port'  # 机器人mirai地址(str)
+AUTHKEY = ''  # 机器人mirai验证密钥(str)
+QQ = 'bot qq'  # 机器人QQ(str)
 
 # 数据库基本配置
-MYSQL_HOST = '127.0.0.1'  # 数据库地址
-MYSQL_PORT = 3306  # 数据库端口
-MYSQL_USER = 'root'  # 数据库用户名
-MYSQL_PWD = '123456'  # 数据库密码
-MYSQL_DB = 'qqbot'  # 数据库名
+MYSQL_HOST = '127.0.0.1'  # 数据库地址(str)
+MYSQL_PORT = 3306  # 数据库端口(int)
+MYSQL_USER = 'root'  # 数据库用户名(str)
+MYSQL_PWD = '123456'  # 数据库密码(str)
+MYSQL_DB = 'qqbot'  # 数据库名(str)
 
 # 变量配置
-ADMIN_USER = [123456]  # 管理员
-ACTIVE_USER = [123456]  # 监听好友
-ACTIVE_GROUP = {123456: '*', 1234567: '*'}  # 监听群聊：前者为群号，后者为指令可用范围（ - 、* 、[qid]）
+ADMIN_USER = [123456]  # 管理员(int)
+ACTIVE_USER = [123456]  # 监听好友(int)
+ACTIVE_GROUP = {123456: '*', 1234567: '*'}  # 监听群聊：前者为群号，后者为指令权限许可范围（ - 、* 、[qid]）(keys.int,values.str)
 
 # github仓库监听配置
-REPO_TIME = 15  # 监听时间间隔
-REPO_NAME = []  # 仓库名
-REPO_API = []  # 仓库api（个数须与REPO_NAME对应）
+REPO_GROUP = []  # 消息推送群聊(int)
+REPO_TIME = 15  # 监听时间间隔(int)
+REPO_NAME = []  # 仓库名(str)
+REPO_API = []  # 仓库api（个数须与REPO_NAME对应）(str)
