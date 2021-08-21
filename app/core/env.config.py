f6ac6354a7fbb833c383c67c3b2164a61c7c1b7b
Colsrch <colsrch@foxmail.com>
2021-08-21 20:53:43 +0800

    优化好友申请，优化定时循环任务
    优化好友申请，支持手动操作申请事件
    优化定时循环任务，未来将支持插件开发者自定义定时循环任务

--- app/core/env.config.py
+++ app/core/env.config.py
@@ -5,6 +5,7 @@
 HOST = 'http://127.0.0.1:8080'  # 机器人mirai地址(str)
 AUTHKEY = ''  # 机器人mirai验证密钥(str)
 QQ = 'bot qq'  # 机器人QQ(str)
+MASTER_QQ = ''  # 主人QQ(str)
 
 # 数据库基本配置
 MYSQL_HOST = '127.0.0.1'  # 数据库地址(str)
@@ -13,6 +14,6 @@ MYSQL_USER = 'root'  # 数据库用户名(str)
 MYSQL_PWD = '123456'  # 数据库密码(str)
 MYSQL_DB = 'qqbot'  # 数据库名(str)
 
-# github仓库监听配置(监听仓库请通过指令配置)
+# github仓库监听配置(监听仓库请通过指令配置) [即将支持在线配置]
 REPO_GROUP = []  # 消息推送群聊(int)
 REPO_TIME = '*/10  * * * *'  # 监听时间间隔(cron表达式)
