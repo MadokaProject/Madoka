1e7b22da60218f7f77ea287036a084716df6bbae
Colsrch <colsrch@foxmail.com>
2021-08-24 14:22:10 +0800

    修复计划任务无法发送消息的bug，将网易云音乐签到模块移至插件内 (#17)
    * 修复无法发送消息的bug
    
    * 将网易云音乐签到模块移至插件内

--- app/extend/schedule.py
+++ app/extend/schedule.py
@@ -5,7 +5,6 @@ from graia.scheduler import timers
 
 from app.core.config import *
 from app.core.settings import LISTEN_MC_SERVER
-from app.extend.NetEaseCloudMusicAction import NetEase_action
 from app.extend.github import github_listener
 from app.extend.mc import mc_listener
 from app.plugin import *
@@ -22,17 +21,13 @@ async def custom_schedule(loop, bcc, bot):
         async def mc_listen_schedule():
             await mc_listener(bot, path, ips, qq, delay)
 
-    @sche.schedule(timers.crontabify("0 8 * * *"))
-    async def NetEase_actions():
-        await NetEase_action(bot)
-
     @sche.schedule(timers.crontabify(REPO_TIME))
     async def github_commit_listener():
         await github_listener(bot)
 
     """计划任务获取接口，该接口用于方便插件开发者设置计划任务"""
     for tasker in base.Schedule.__subclasses__():
-        obj = tasker()
+        obj = tasker(bot)
         if obj.cron:
             @sche.schedule(timers.crontabify(obj.cron))
             async def Tasker():
