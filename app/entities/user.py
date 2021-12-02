75a0abbbdc4e91a0a2826e24e8d1affbdd97cda5
Colsrch <colsrch@foxmail.com>
2021-12-03 00:18:16 +0800

    新增积分【搬砖、打工、踢】新玩法

--- app/entities/user.py
+++ app/entities/user.py
@@ -75,3 +75,87 @@ class BotUser:
                 [self.qq]
             )
             return res[0][0]
+
+    def kick(self, src, dst, point, num) -> bool:
+        """踢
+            :param src: 来源QQ
+            :param dst: 目标QQ
+            :param point: 掉落积分
+            :param num: 每天最多次数
+        """
+        with MysqlDao() as db:
+            res = db.query(
+                "SELECT COUNT(*) FROM kick WHERE src=%s AND dst=%s AND TO_DAYS(time)=TO_DAYS(now())",
+                [src, dst]
+            )
+            if not res[0][0] < num:
+                return False
+            self.update_point(point)
+            res = db.update(
+                "INSERT INTO kick (src, dst, time, point) VALUES (%s, %s, NOW(), %s)",
+                [src, dst, -point]
+            )
+            if not res:
+                raise Exception()
+        return True
+
+    def steal(self, src, dst, point, num) -> bool:
+        """偷对方积分
+            :param src: 来源QQ
+            :param dst: 目标QQ
+            :param point: 偷取积分
+            :param num: 每天最多次数
+        """
+        with MysqlDao() as db:
+            res = db.query(
+                "SELECT COUNT(*) FROM steal WHERE src=%s AND dst=%s AND TO_DAYS(time)=TO_DAYS(now())",
+                [src, dst]
+            )
+            if not res[0][0] < num:
+                return False
+            self.update_point(point)
+            res = db.update(
+                "INSERT INTO steal (src, dst, time, point) VALUES (%s, %s, NOW(), %s)",
+                [src, dst, point]
+            )
+            if not res:
+                raise Exception()
+        return True
+
+    def get_moving_bricks_status(self) -> bool:
+        """查询搬砖状态"""
+        with MysqlDao() as db:
+            res = db.query(
+                "SELECT COUNT(*) FROM user WHERE uid=%s AND last_moving_bricks=CURDATE()",
+                [self.qq]
+            )
+            return res[0][0]
+
+    def moving_bricks(self):
+        """搬砖"""
+        with MysqlDao() as db:
+            res = db.update(
+                "UPDATE user SET points=points+%s, last_moving_bricks=CURDATE() WHERE uid=%s",
+                [self.point, self.qq]
+            )
+            if not res:
+                raise Exception()
+
+    def get_work_status(self) -> bool:
+        """查询打工状态"""
+        with MysqlDao() as db:
+            res = db.query(
+                "SELECT COUNT(*) FROM user WHERE uid=%s AND last_part_time_job=CURDATE()",
+                [self.qq]
+            )
+            return res[0][0]
+
+    def work(self):
+        """打工"""
+        with MysqlDao() as db:
+            res = db.update(
+                "UPDATE user SET points=points+%s, last_part_time_job=CURDATE() WHERE uid=%s",
+                [self.point, self.qq]
+            )
+            if not res:
+                raise Exception()
