07939a7ea1e14bbef58a3622be6e527a630bb8cf
Colsrch <colsrch@foxmail.com>
2022-01-06 15:29:26 +0800

    完善版本监控

--- app/util/version.py
+++ app/util/version.py
@@ -1,3 +1,4 @@
+import aiohttp.client
 from graia.ariadne.app import Ariadne
 from graia.ariadne.message.chain import MessageChain
 from graia.ariadne.message.element import Plain
@@ -8,26 +9,29 @@ from app.core.config import Config
 
 
 async def version_notice(app: Ariadne, config: Config):
-    remote_version_url = 'https://cdn.jsdelivr.net/gh/MadokaProject/Application@feature/colsrch/version/app/util/version.json'
+    remote_version_url = 'https://cdn.jsdelivr.net/gh/MadokaProject/Application@master/app/util/version.json'
     logger.info(f'欢迎使用{config.INFO_NAME}')
     logger.info(f'Docs: {config.INFO_DOCS}')
     logger.info(f'Repo: {config.INFO_REPO}')
     logger.info(f'Version: {config.INFO_VERSION}')
-    remote_info = await doHttpRequest(remote_version_url, method='get', _type='json')
-    remote_version = remote_info['version']
-    remote_update_logs = remote_info['update_log']
-    logger.info(f'Remote version: {remote_version}')
-    if remote_version > config.INFO_VERSION:
-        log_msg = ''
-        for log in remote_update_logs:
-            if log['version'] > config.INFO_VERSION:
-                log_msg += f"{log['version']}: {log['info']}\r\n"
-            else:
-                break
-        await app.sendFriendMessage(config.MASTER_QQ, MessageChain.create([
-            Plain('当前不是最新版本，建议更新\r\n'),
-            Plain('更新提要：\r\n'),
-            Plain(log_msg),
-            Plain(f'详情内容请前往{config.INFO_REPO}查看')
-        ]))
-        await app.sendFriendMessage(config.MASTER_QQ, MessageChain.create([Plain('发送.p u可进行更新操作')]))
+    try:
+        remote_info = await doHttpRequest(remote_version_url, method='get', _type='json')
+        remote_version = remote_info['version']
+        remote_update_logs = remote_info['update_log']
+        logger.info(f'Remote version: {remote_version}')
+        if remote_version > config.INFO_VERSION:
+            log_msg = ''
+            for log in remote_update_logs:
+                if log['version'] > config.INFO_VERSION:
+                    log_msg += f"{log['version']}: {log['info']}\r\n"
+                else:
+                    break
+            await app.sendFriendMessage(config.MASTER_QQ, MessageChain.create([
+                Plain('当前不是最新版本，建议更新\r\n'),
+                Plain('更新提要：\r\n'),
+                Plain(log_msg),
+                Plain(f'详情内容请前往{config.INFO_REPO}/releases查看')
+            ]))
+            await app.sendFriendMessage(config.MASTER_QQ, MessageChain.create([Plain('发送.p u可进行更新操作')]))
+    except aiohttp.client.ClientConnectorError:
+        logger.warning('获取远程版本信息超时')
