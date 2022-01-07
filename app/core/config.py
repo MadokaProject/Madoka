25a904e7cdfc54454bb06943022f0552ac3841f6
Colsrch <colsrch@foxmail.com>
2022-01-07 22:15:17 +0800

    优化配置文件

--- app/core/config.py
+++ app/core/config.py
@@ -14,9 +14,9 @@ class Config:
         self.INFO_DOCS = 'https://madoka.colsrch.cn'
         self.INFO_REPO = 'https://github.com/MadokaProject/Application'
 
-        self.LOGIN_HOST = self.cf.get('bot', 'login_host', fallback='127.0.0.1')
-        self.LOGIN_PORT = self.cf.get('bot', 'login_port', fallback='8080')
-        self.LOGIN_QQ = self.cf.get('bot', 'login_qq')
+        self.LOGIN_HOST = self.cf.get('bot', 'host', fallback='127.0.0.1')
+        self.LOGIN_PORT = self.cf.get('bot', 'port', fallback='8080')
+        self.LOGIN_QQ = self.cf.get('bot', 'qq')
         self.VERIFY_KEY = self.cf.get('bot', 'verify_key')
         self.BOT_NAME = self.cf.get('bot', 'bot_name')
         self.MASTER_QQ = self.cf.get('bot', 'master_qq')
@@ -24,19 +24,19 @@ class Config:
         self.DEBUG = True if self.cf.get('bot', 'debug', fallback='False').lower() == 'true' else False
         self.ONLINE = True if self.cf.get('bot', 'online', fallback='True').lower() == 'true' else False
 
-        self.MYSQL_HOST = self.cf.get('mysql', 'mysql_host', fallback='127.0.0.1')
-        self.MYSQL_PORT = self.cf.getint('mysql', 'mysql_port', fallback=3306)
-        self.MYSQL_USER = self.cf.get('mysql', 'mysql_user')
-        self.MYSQL_PWD = self.cf.get('mysql', 'mysql_pwd')
-        self.MYSQL_DATABASE = self.cf.get('mysql', 'mysql_database')
+        self.MYSQL_HOST = self.cf.get('mysql', 'host', fallback='127.0.0.1')
+        self.MYSQL_PORT = self.cf.getint('mysql', 'port', fallback=3306)
+        self.MYSQL_USER = self.cf.get('mysql', 'user')
+        self.MYSQL_PWD = self.cf.get('mysql', 'password')
+        self.MYSQL_DATABASE = self.cf.get('mysql', 'database')
 
         self.COMMON_RETENTION = self.cf.get('log', 'commonRetention', fallback='7')
         self.ERROR_RETENTION = self.cf.get('log', 'errorRetention', fallback='30')
 
-        self.COIN_NAME = self.cf.get('coin_settings', 'coin_name', fallback='金币')
+        self.COIN_NAME = self.cf.get('coin_settings', 'name', fallback='金币')
 
         self.REPO_ENABLE = True if self.cf.get('github', 'enable', fallback='False').lower() == 'true' else False
-        self.REPO_TIME = self.cf.get('github', 'repo_time', fallback='*/10  * * * *')
+        self.REPO_TIME = self.cf.get('github', 'time', fallback='*/10  * * * *')
 
     def change_debug(self):
         if not self.ONLINE:
