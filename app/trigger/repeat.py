b6547cb320a7ec41bb7988fe45c3aaad29ea6b5c
Colsrch <colsrch@foxmail.com>
2022-03-06 20:24:04 +0800

    修复消息数量不足时，复读模块报错的bug

--- app/trigger/repeat.py
+++ app/trigger/repeat.py
@@ -15,11 +15,14 @@ class Repeat(Trigger):
         if self.msg[0][0] in '.,;!?。，；！？/\\':  # 判断是否为指令
             return
         probability = random.randint(0, 101)
-        if (probability < 1) and repeated(self.group.id, self.app.account, 2):
-            await self.app.sendGroupMessage(self.group, self.message.asSendable())
-            save(self.group.id, self.app.account, self.message.asDisplay())
-            logger.info('Random Repeat: ' + self.message.asDisplay())
-        if repeated(self.group.id, self.app.account, 2):
-            await self.app.sendGroupMessage(self.group, self.message.asSendable())
-            save(self.group.id, self.app.account, self.message.asDisplay())
-            logger.info('Follow Repeat: ' + self.message.asDisplay())
+        try:
+            if (probability < 1) and repeated(self.group.id, self.app.account, 2):
+                await self.app.sendGroupMessage(self.group, self.message.asSendable())
+                save(self.group.id, self.app.account, self.message.asDisplay())
+                logger.info('Random Repeat: ' + self.message.asDisplay())
+            if repeated(self.group.id, self.app.account, 2):
+                await self.app.sendGroupMessage(self.group, self.message.asSendable())
+                save(self.group.id, self.app.account, self.message.asDisplay())
+                logger.info('Follow Repeat: ' + self.message.asDisplay())
+        except Exception as e:
+            logger.warning(e)
