539f230585dd63ebc3b141948e5f61aed8005456
Colsrch <colsrch@foxmail.com>
2022-08-28 23:27:28 +0800

    兼容 sqlite

--- app/plugin/basic/__06_permission/database/20220819-2200.sql
+++ app/plugin/basic/__06_permission/database/20220819-2200.sql
@@ -1,6 +1,18 @@
-ALTER TABLE user DROP english_answer;
-ALTER TABLE user DROP last_login;
-ALTER TABLE user DROP points;
-ALTER TABLE user DROP signin_points;
-ALTER TABLE user MODIFY uid char(12) not null;
-ALTER TABLE `group` ADD PRIMARY KEY (uid);
+mysql: ALTER TABLE user DROP english_answer;
+mysql: ALTER TABLE user DROP last_login;
+mysql: ALTER TABLE user DROP points;
+mysql: ALTER TABLE user DROP signin_points;
+mysql: ALTER TABLE user MODIFY uid char(12) not null;
+mysql: ALTER TABLE `group` ADD PRIMARY KEY (uid);
+
+sqlite: ALTER TABLE user RENAME TO _user_old;
+sqlite: CREATE TABLE user as SELECT id, uid, active, level, permission FROM _user_old;
+sqlite: DROP TABLE _user_old;
+sqlite: ALTER TABLE `group` RENAME TO _group_old;
+sqlite: CREATE TABLE `group` (
+    uid CHAR(12) NOT NULL PRIMARY KEY,
+    permission VARCHAR(512) NOT NULL,
+    active INT NOT NULL
+);
+sqlite: INSERT INTO `group` SELECT uid, permission, active FROM _group_old;
+sqlite: DROP TABLE _group_old;
--- app/plugin/basic/__10_mc_info/database/20220819-2200.sql
+++ app/plugin/basic/__10_mc_info/database/20220819-2200.sql
@@ -1,3 +1,16 @@
-ALTER TABLE mc_server CHANGE ip host VARCHAR(255) NOT NULL;
-ALTER TABLE mc_server MODIFY report VARCHAR(255) NULL;
-ALTER TABLE mc_server ADD PRIMARY KEY (host, port);
+mysql: ALTER TABLE mc_server CHANGE ip host VARCHAR(255) NOT NULL;
+mysql: ALTER TABLE mc_server MODIFY report VARCHAR(255) NULL;
+mysql: ALTER TABLE mc_server ADD PRIMARY KEY (host, port);
+
+sqlite: ALTER TABLE mc_server RENAME TO _mc_server_old;
+sqlite: CREATE TABLE mc_server (
+    host VARCHAR(255) NOT NULL,
+    port char(5) NOT NULL,
+    report VARCHAR(255) NULL,
+    listen INTEGER DEFAULT 0,
+    `default` INTEGER DEFAULT 0,
+    delay INTEGER DEFAULT 60,
+    PRIMARY KEY (host, port)
+);
+sqlite: INSERT INTO mc_server SELECT ip, port, report, listen, `default`, delay FROM _mc_server_old;
+sqlite: DROP TABLE _mc_server_old;
