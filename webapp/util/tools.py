d918487d54a6fda33e66f98c024e8cd394c06865
Colsrch <colsrch@foxmail.com>
2022-09-17 01:26:30 +0800

    暂时移除 webapp

--- webapp/util/tools.py
+++
@@ -1,7 +0,0 @@
-def GET_RET_JSON(code_id, msg=None, data=None):
-    ret = {'code': code_id, 'msg': 'null'}
-    if msg:
-        ret['msg'] = msg
-    if data or data == {}:
-        ret.update({'data': data})
-    return ret
